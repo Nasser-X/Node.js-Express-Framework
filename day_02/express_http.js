835e293f74622a468e0a6c28291d1f97fa41f1f3
Nasser X <nao1422@gmail.com>
2018-03-19 19:47:54 +0300

    Day 02: Update Server

--- day_02/express_http.js
+++ day_02/express_http.js
@@ -1,11 +1,11 @@
-const express = require ('express');
-const app = express ();
+const express = require('express');
+const app = express();
 const port = 3000;
 
-app.get ('/', (request, response) => {
-    response.setHeader('Content-type', 'text/html')
-    response.statusCode = 200
-    response.send (`
+app.get('/', (request, response) => {
+   response.setHeader('Content-type', 'text/html')
+   response.statusCode = 200
+   response.send(`
     <html>
         <head><title>Welcome to my website</title></head>
         <body><h1>This is the first page</h1></body>
@@ -13,10 +13,10 @@ app.get ('/', (request, response) => {
     `);
 });
 
-app.listen (port, err => {
-  if (err) {
-    return console.log ('something bad happened', err);
-  }
+app.listen(port, err => {
+   if (err) {
+      return console.log('something bad happened', err);
+   }
 
-  console.log (`server is listening on ${port}`);
+   console.log(`server is listening on ${port}`);
 });
